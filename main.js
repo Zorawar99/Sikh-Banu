f45bd3135c8220b6f6a83a89b71affe758589ee2
Zorawar99 <37868046+Zorawar99@users.noreply.github.com>
2018-06-01 05:11:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,70 @@
 var custom=document.getElementById('custom');
-var thisPage=window.location.href;
 var openButton=document.getElementById('openButton');
-var page=thisPage.slice(43,48);
-setInterval(function() {
-       var x="s1";
-       var y="s2";
-      document.getElementById(y).style.position="relative";
-      document.getElementById(x).style.display="none";
+var page= document.getElementsByClassName('path');
+//       setInterval(function(){
       
-},5000)
+//             document.getElementById('s2').style.position="relative";
+//             document.getElementById('s1').style.display="none";
+            
+//       },10000)
+
+
+// setInterval(function(){
+//       document.getElementById('s3').style.position="relative";
+//       document.getElementById('s2').style.display="none";
+// },20000)
+
+// setInterval(function(){
+//       document.getElementById('s4').style.position="relative";
+//       document.getElementById('s3').style.display="none";
+// },30000)
+// setInterval(function(){
+//       document.getElementById('s5').style.position="relative";
+//       document.getElementById('s4').style.display="none";
+// },40000)
+// setInterval(function(){
+//       document.getElementById('s6').style.position="relative";
+//       document.getElementById('s5').style.display="none";
+// },50000)
+// setInterval(function(){
+//       document.getElementById('s7').style.position="relative";
+//       document.getElementById('s6').style.display="none";
+// },60000)
+// setInterval(function(){
+//       document.getElementById('s8').style.position="relative";
+//       document.getElementById('s7').style.display="none";
+// },70000)
+// setInterval(function(){
+//       document.getElementById('s9').style.position="relative";
+//       document.getElementById('s8').style.display="none";
+// },80000)
+// setInterval(function(){
+//       document.getElementById('s10').style.position="relative";
+//       document.getElementById('s9').style.display="none";
+// },90000)
+// setInterval(function(){
+//       document.getElementById('10').style.position="relative";
+//       document.getElementById('s9').style.display="none";
+// },100000)
+
+
 if(!localStorage.getItem('bgcolor')){
       changeBgColor();
 }
 else {
       styleBgColor();
 }
-if(!localStorage.getItem('theme')){
-      changeTheme();
+if(!localStorage.getItem('fontcolor')){
+      changeFontColor();
 }
 else {
-      styleTheme();
+      styleFontColor();
+}
+if(!localStorage.getItem('fonttype')){
+      changeFontType();
+}
+else {
+      styleFontType();
 }
 if(!localStorage.getItem('Size')) {
       changeFontSizeS();
@@ -29,7 +74,7 @@ else {
 }
 function showCustomize() {
   custom.style.transition=" all 1.5s ease-in-out";
-  custom.style.maxHeight="375px";
+  custom.style.maxHeight="500px";
 }
 function closeCustomize() {
  custom.style.transition=" all 1.5s ease-in-out";
@@ -43,71 +88,7 @@ function bringButton() {
  openButton.style.transition="all 1.5s ease-in-out";      
  openButton.style.opacity="1";
 }
-function changeTheme() {
-      localStorage.setItem('theme',document.getElementById('theme1').value);
-      styleTheme();
-}
-function styleTheme() {
- var currentColor= localStorage.getItem('theme');
- document.getElementById('theme1').value= currentColor;
-    
- switch(document.getElementById('theme1').value){
-            case "Daracula":
-            {
-                  document.body.style.transition="all 1.5s ease-in-out";
-                  document.getElementById(page).style.transition="all 1.5s ease-in-out";
-                  custom.style.color="#000016";
-                  custom.style.transition="all 1.5s ease-in-out";
-                  custom.style.backgroundColor="#fff";
-                  document.body.style.backgroundColor="#000016";
-                  document.body.style.color="#fff";
-                  openButton.style.backgroundColor="#000016";
-                  openButton.style.transition="all 1.5s ease-in-out";
-                  openButton.style.color="#fff";
-                  document.getElementById(page).style.backgroundColor="#000016";            
-                  document.getElementById(page).style.color="#fff";            
-            break;
-            }
-            case "Yellow":
-            {     
-                  document.body.style.transition="all 1.5s ease-in-out";
-                  document.getElementById(page).style.transition="all 1.5s ease-in-out";
-                  document.body.style.backgroundColor="yellow";
-                  document.getElementById(page).style.backgroundColor="yellow";
-                  
-                  openButton.style.color="#000016";
-                  openButton.style.backgroundColor="yellow";
-                  document.getElementById(page).style.color="#000016";                  
-                  document.body.style.color="#000016";
-                  break;
-            
-            }
-            case "White":
-            {
-                  document.body.style.transition="all 1.5s ease-in-out";
-                  document.getElementById(page).style.transition="all 1.5s ease-in-out";
-                  openButton.style.color="black";    
-                  openButton.style.backgroundColor="#fff";
-                  document.body.style.backgroundColor="#fff";
-                  document.getElementById(page).style.backgroundColor="#fff";
-                  document.getElementById(page).style.color="#000016";
-                  document.body.style.color="#000016";
-                  break;
-            }
-           case "Blue":
-            {
-                  document.body.style.transition="all 1.5s ease-in-out";
-                  document.getElementById(page).style.transition="all 1.5s ease-in-out";
-                  openButton.style.color="#000016";
-                  document.body.style.backgroundColor="lightskyblue";
-                  openButton.style.backgroundColor="lightskyblue";
-                  document.getElementById(page).style.backgroundColor="lightskyblue";                  
-                  document.getElementById(page).style.color="#000016";                  
-                  document.body.style.color="#000016";
-                  break;
-            }
-      }
-}
+
 
 function openLeftBar() {
       document.getElementById("leftbar").style.left="0";
@@ -121,9 +102,7 @@ function fadeButtons() {
            i[j].style.opacity="0";
      }
 }
-function fadeTheme() {
-      document.getElementById("themetoggle").style.opacity="0";
-}
+
 function fadeLeftHam() {
       document.getElementById('ham').style.transition="all 0.5s ease-in-out";
       document.getElementById('ham').style.opacity="0";
@@ -141,117 +120,69 @@ function closeLeftBar() {
  document.getElementById('leftbar').style.left="-200px";
 }
 
-function bringThemeBack() {
-      document.getElementById("themetoggle").style.opacity="1";
-}
+
 function bringLeftHamBack() {
       document.getElementById('ham').style.transition="all 0.5s ease-in-out";
       document.getElementById('ham').style.opacity="1";
 }
-
-function fadeFontSize() {
-      document.getElementById('fontSize1').style.transition="opacity 0.5s ease-in-out";
-      document.getElementById('fontSize1').style.opacity="0";
-//      setTimeout(function(){
-//            document.getElementById('fontSize1').style.width="0";
-//            document.getElementById('fontSize1').style.height="0";},1000);
-}
-function bringFontSize() {
-      document.getElementById('fontSize1').style.transition="all 0.5s ease-in-out";
-      document.getElementById('fontSize1').style.opacity="1";
-      // setTimeout(function bf(){
-      //       document.getElementById('fontSize1').style.width="250px";
-      //       document.getElementById('fontSize1').style.height="auto";},100);
- }
 function changeFontColor() {
-      var i = document.getElementById('fontColor').value;
-        document.getElementById(page).style.transition="all .5s ease-in-out";
-        document.getElementById(page).style.color=i;
+      localStorage.setItem('fontcolor',document.getElementById('fontColor').value);
+      styleFontColor();
 }
 function changeBgColor() {
       localStorage.setItem('bgcolor',document.getElementById('bgColor').value);
       styleBgColor();
 }
+function styleFontColor() {                 
+      var currentColor = localStorage.getItem('fontcolor');
+      document.getElementById('fontColor').value = currentColor;
+      document.body.style.transition="all 1.5s ease-in-out" ;
+      document.body.style.color= currentColor ;
+      for (var i=0;i<page.length;i++)
+      {
+            page[i].style.transition="all 1.5s ease-in-out";
+            page[i].style.color=currentColor;
+      }    
+}
 
  function styleBgColor() {                 
        var currentColor = localStorage.getItem('bgcolor');
        document.getElementById('bgColor').value = currentColor;
-       document.body.style.backgroundColor= currentColor ;
        document.body.style.transition="all 1.5s ease-in-out" ;
-       document.getElementById(page).style.backgroundColor=currentColor;
+       document.body.style.backgroundColor= currentColor ;
        openButton.style.backgroundColor=currentColor;
-       document.getElementById(page).style.transition="all 1.5s ease-in-out";
+       for (var i=0;i<page.length;i++)
+       {
+             page[i].style.transition="all 1.5s ease-in-out";
+             page[i].style.backgroundColor=currentColor;
+       }
+
        
 }
 function changeFontType() {
-     var i =document.getElementById('changeFont').value;
-     document.getElementById(page).style.fontFamily=i;
+      localStorage.setItem('fonttype',document.getElementById('changeFont').value);
+      styleFontType();
 }
-function changeFontColorS() {
-      var i = document.getElementById('fontColorS').value;
-        document.getElementById(page).style.transition="all .5s ease-in-out";
-        document.getElementById(page).style.color=i;
-}
-function changeBgColorS() {
-      var i = document.getElementById('bgColorS').value;
-      document.body.style.backgroundColor= i ;
-      document.body.style.transition="all 1.5s ease-in-out" ;
-      document.getElementById(page).style.backgroundColor=i;
-      openButton.style.backgroundColor=i;
-      document.getElementById(page).style.transition="all 1.5s ease-in-out";
+function styleFontType() {
+      var currentType= localStorage.getItem('fonttype');
+      document.getElementById('changeFont').value = currentType;
+      for (var i=0;i<page.length;i++)
+      {
+            page[i].style.transition="all 1.5s ease-in-out";
+            page[i].style.fontFamily=currentType;
+      }
 }
 function changeFontSize() {
-     var i = document.getElementById('fontselect').value;
-     document.getElementById(page).style.transition="all 1.5s ease-in-out";
-     document.getElementById(page).style.fontSize= i;
-     if(i=="29px"||i=="35px"||i=="24px") {
-      document.body.style.overflow="scroll";
-    }else {
-          document.body.style.overflow="hidden";
-    }
-}
-function changeFontSizeS() {
-      localStorage.setItem('Size',document.getElementById('fontsize').value);
+      localStorage.setItem('Size',document.getElementById('fontselect').value);
       styleFontSize();
 }
 function styleFontSize() {
       var currentSize= localStorage.getItem('Size');
-      document.getElementById('fontsize').value = currentSize;
-      var i = document.getElementById('fontsize').value;
-      document.getElementById(page).style.transition="all 1.5s ease-in-out";
-      document.getElementById(page).style.fontSize= i;
-      if(i=="29px"||i=="35px"||i=="24px") {
-        document.body.style.overflow="scroll";
-      }else {
-            document.body.style.overflow="hidden";
-      }
-      switch (i) {
-            case "3px":
-            document.getElementById(page).style.width="5%";
-            case "6px":
-            document.getElementById(page).style.width="10%";
-            case "12px":
-            document.getElementById(page).style.width="20%";
-            case "18px":
-            document.getElementById(page).style.width="30%";
-            case "24px":
-            document.getElementById(page).style.width="50%";
-            case "29px":
-            document.getElementById(page).style.width="55%";
-            case "35px":
-            document.getElementById(page).style.width="60%";
+      document.getElementById('fontselect').value = currentSize;
+      var i = document.getElementById('fontselect').value;
+      for (var i=0;i<page.length;i++)
+      {
+            page[i].style.transition="all 1.5s ease-in-out";
+            page[i].style.fontSize=currentSize;
       }
  }
- function changeFontSizeP() {
-       var i=document.getElementById('fontSizeP').value;
-       i+="px";
-       document.getElementById(page).style.fontSize=i;
-       if(i>="30px")
-       {
-             document.body.style.overflow="scroll";
-       }
-      }
-function changeFontTypeP() {
-      var i=document.getElementById('changeFontTypeP').value;
-      document.getElementById(page).style.fontFamily=i;
-}
